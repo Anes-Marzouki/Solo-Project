8dc525ee94f367c91d313b80f2a5585c394b6a88
Anes Marzouki <marzoukianas@gmail.com>
2019-11-08 10:49:55 +0100

    Finishes the basing click on square fnc

--- main.js
+++ main.js
@@ -1,21 +1,92 @@
 $( document ).ready(function() {
 	// console.log('hi');
-	$('#right').on('click', function(){
-		$('.square').css('left', '+=10');
-	})
+//set of Variables
 
-	$('#left').on('click', function(){
-		$('.square').css('left', '-=10');
-	})
+	var timer = null;//set it to undefined ? or null?
+	var startGame = false;
+ 	var score = $(".score").text();
+	var nowTime = $(".timer").text(); //global from <div>text like 30s
+//logs
+console.log(score);
+console.log(nowTime)
 
-	$(document).keydown(function(event){
-		console.log(event.keyCode)
-		// var key = event.keyCose;
-		if (event.keyCode == 39){
-				$('.square').css('left', '+=10');
-		}else if (event.keyCode == 37){
-			$('.square').css('left', '-=10');
+	//Base skelaton for the mini game!
+//generates random number for the move function.
+	function randomNumb(min,max){
+	    return Math.floor( Math.random() * ( max - min ) + min + 1 );
+            //test this return otherwise try (max - min + 1).
+	}
+//timer
+	//start the timer with a function maybe .onclick start timer with a startgame var set to true?
+		// like if startgame is false .click on element with class start ?? $('.start').click(f() {start the timer and sets startgame to true}) 
+	$('.square').on('click', function (){
+		if (!startGame) {
+			timerFn();
+			startGame = true;
 		}
+	})
+//starts the timer
+	function timerFn(){
+	  	timer = setInterval( function(){
+		    nowTime--; // 30s--
+		    $(".timer").html(nowTime);
+		    if(nowTime < 1) {
+		      clearInterval(timer);
+		      stop();
+		    }
+	    },1000);
+	}
+//clicking on square (element:maybe change it to an img) calls the move function below
+	$(".square").click(function(){
+	  updateScore();  
+	  move( $(this) ); //this refers to the square.
+	});
+	//move!!
+		function move(element){
+		  var left = randomNumb(2,75);
+		  $(element).css("left", left + "%");
+	          //for now this moves the elmt back and forth
+	          //using the CSS' left value, try and implement
+	          //a more dynamic movement pattern.
+		}
+	//increases score var
+		function updateScore(){
+		  score++;
+		  $(".score").html(score);
+		}
+//stop and adds score, an ending mechanic.
+	function stop(){
+	  $(".square").addClass("stop"); // stop pointer event
+	  $(".score").html("Your score is: " + score);
+	}
+})
+
+
+
+
+//-----------------------click-events-&-keypress-events--------------------------------
+
+
+
+
+
+
+	// $('#right').on('click', function(){
+	// 	$('.square').css('left', '+=10');
+	// })
+
+	// $('#left').on('click', function(){
+	// 	$('.square').css('left', '-=10');
+	// })
+
+	// $(document).keydown(function(event){
+	// 	console.log(event.keyCode)
+	// 	// var key = event.keyCose;
+	// 	if (event.keyCode == 39){
+	// 			$('.square').css('left', '+=10');
+	// 	}else if (event.keyCode == 37){
+	// 		$('.square').css('left', '-=10');
+	// 	}
 	// 	switch(event.keyCode){
 	// 		case 32: //space
 	// 		return function shoot (){
@@ -32,88 +103,32 @@ $( document ).ready(function() {
 	// 		case 37: //left
 	// 			$('.square').css('left', '-=10');
 	// 	}
-	})
+	// })
 	
 
 
-})	
+
 
 
 
 //----------------------------
-/*
-If key presses anywhere need to be caught
-(for example, to implement global shortcut keys on a page),
-it is useful to attach this behavior to the document object.
-Because of event bubbling, all key presses will make their way up the
-DOM to the document object unless explicitly stopped.
+
+// If key presses anywhere need to be caught
+// (for example, to implement global shortcut keys on a page),
+// it is useful to attach this behavior to the document object.
+// Because of event bubbling, all key presses will make their way up the
+// DOM to the document object unless explicitly stopped.
 
 
-event.keyCode {
-	32: space
-	37: Left key
-	38: Up key
-    39: Right key         
-    40: Down key
-    } 
-
-//Base skelaton for the mini game!
-
-//timer
- 	var score = $(".score").text(),
-	var currentTime = $(".timer").text(); //global from <div>text like 30s
-
-//start the timer with a function maybe .onclick start timer with a startgame var set to true?
-	like if startgame is false .click on element with class start ?? $('.start').click(f() {start the timer and sets startgame}) 
+// event.keyCode {
+// 	32: space
+// 	37: Left key
+// 	38: Up key
+//     39: Right key         
+//     40: Down key
+//     } 
 
 
-//starts the timer
-	var timer; //global set it to undefined ? or null?
-	function timer(){
-	  	timer = setInterval( function(){
-		    currentTime--; // 30s--
-		    $(".timer").html(currentTimer);
-		    if(currentTime < 1){
-		      clearInterval(timer);
-		      end();
-		    }
-	    },1000);
-	}
-
-//clicking on square (element:maybe change it to an img) calls the move function below
-	$(".square").click(function(){
-	  updateScore();  
-	  move( $(this) ); //this refers to the square.
-	});
-
-//move!!
-	function move(element){
-	  var left = randomNumb(2,75);
-	  $(element).css("left", left + "%");
-          //for now this moves the elmt back and forth
-          //using the CSS' left value, try and implement
-          //a more dynamic movement pattern.
-	}
-
-//generates random number for the move function.
-	function randomNumb(min,max){
-	    return Math.floor( Math.random() * ( max - min ) + min + 1 );
-            //test this return otherwise try (max - min + 1).
-	}
-
-
-//increases score var
-	function updateScore(){
-	  score++;
-	  $(".score").html(score);
-	}
-
-//stop and adds score, an ending mechanic.
-	function stop(){
-	  $(".ball").addClass("stop"); // stop pointer event
-	  $(".score").html("Your score is: " + score);
-	}
-*/
 
 //This doesn't work, you can't nest an if statement inside a case!!!
 	// $(document).keydown(function(event){
