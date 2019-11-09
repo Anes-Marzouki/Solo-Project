b033cf18a24f4c85db40acbe24e484dd530456bd
AnesMarzouki <56113177+Anes-Marzouki@users.noreply.github.com>
2019-11-09 07:10:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function () { //The fancier version of document.ready()
 
     //variables for selection.
     var container = $('#container');
@@ -105,10 +105,10 @@ $(function () {
     });
 
     function collision($div1, $div2) {
-        var x1 = $div1.offset().left;
-        var y1 = $div1.offset().top;
-        var h1 = $div1.outerHeight(true);
-        var w1 = $div1.outerWidth(true);
+        var x1 = $div1.offset().left;//x axis
+        var y1 = $div1.offset().top;//y axis
+        var h1 = $div1.(true);
+        var w1 = $div1.outerWidth(true);//passing true to return margin.
         var b1 = y1 + h1;
         var r1 = x1 + w1;
         var x2 = $div2.offset().left;
@@ -118,8 +118,10 @@ $(function () {
         var b2 = y2 + h2;
         var r2 = x2 + w2;
 
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-        return true;
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+          return false;
+        }else {
+          return true;}
     }
 
 
