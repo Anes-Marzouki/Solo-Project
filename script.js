9a2df760b7ff68ca46cffa9d18c039d435c2634f
AnesMK <marzoukianas@gmail.com>
2019-11-09 08:58:01 +0100

    kind of

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(function () { //The fancier version of document.ready()
+$(function () {
 
-    //variables for selection.
+    //saving dom objects to variables
     var container = $('#container');
-    var square = $('#square');//the element, can be an image.
+    var square = $('#square');
     var pole = $('.pole');
     var pole_1 = $('#pole_1');
     var pole_2 = $('#pole_2');
@@ -10,7 +10,7 @@ $(function () { //The fancier version of document.ready()
     var speed_span = $('#speed');
     var restart_btn = $('#restart_btn');
 
-    //initial Vals of container, pole and square.
+    //saving some initial setup
     var container_width = parseInt(container.width());
     var container_height = parseInt(container.height());
     var pole_initial_position = parseInt(pole.css('right'));
@@ -19,7 +19,7 @@ $(function () { //The fancier version of document.ready()
     var square_height = parseInt(square.height());
     var speed = 10;
 
-    //some other inits
+    //some other declarations
     var go_up = false;
     var score_updated = false;
     var game_over = false;
@@ -35,7 +35,7 @@ $(function () { //The fancier version of document.ready()
 
             var pole_current_position = parseInt(pole.css('right'));
 
-            //update score when the poles have passed the square
+            //update the score when the poles have passed the square successfully
             if (pole_current_position > container_width - square_left) {
                 if (score_updated === false) {
                     score.text(parseInt(score.text()) + 1);
@@ -43,7 +43,7 @@ $(function () { //The fancier version of document.ready()
                 }
             }
 
-            //check whether the poles went out of the window
+            //check whether the poles went out of the container
             if (pole_current_position > container_width) {
                 var new_height = parseInt(Math.random() * 100);
 
@@ -69,6 +69,18 @@ $(function () { //The fancier version of document.ready()
         }
 
     }, 40);
+
+
+$('#container').on('mousedown', function () { //Mouse click On
+        if (go_up === false && game_over === false) {
+            go_up = setInterval(up, 50);
+        }
+    }); 
+
+$('#container').on('mouseup', function () { //Mouse click On
+            clearInterval(go_up);
+            go_up = false;
+    }); 
 
     $(document).on('keydown', function (e) {
         var key = e.keyCode;
@@ -105,10 +117,10 @@ $(function () { //The fancier version of document.ready()
     });
 
     function collision($div1, $div2) {
-        var x1 = $div1.offset().left;//x axis
-        var y1 = $div1.offset().top;//y axis
-        var h1 = $div1.(true);
-        var w1 = $div1.outerWidth(true);//passing true to return margin.
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
         var b1 = y1 + h1;
         var r1 = x1 + w1;
         var x2 = $div2.offset().left;
@@ -118,15 +130,14 @@ $(function () { //The fancier version of document.ready()
         var b2 = y2 + h2;
         var r2 = x2 + w2;
 
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-          return false;
-        }else {
-          return true;}
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+        return true;
     }
 
 
 
 });
+
 
 
 
